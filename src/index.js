aea1990cca14c86a78d813c803efc1a4df6233fb
Ofentse30 <ofentseknowledge3@gmail.com>
2024-09-20 09:21:52 -0700

    added content for the search engine to be active

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-form");
+  let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
 }
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("Submit", handleSearchSubmit);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
