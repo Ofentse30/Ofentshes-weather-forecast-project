c7f5dea6407355d48bb6ab0f5d5fb955d8d5970c
Ofentse30 <ofentseknowledge3@gmail.com>
2024-09-29 11:41:37 -0700

    Changed CSS

--- src/index.js
+++ src/index.js
@@ -53,38 +53,6 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
-  function getForecast(city) {
-    let apiKey = "e6d09442ad1b12tdob2ebfd0f8dc323b";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-    axios(apiUrl).then(displayForecast);
-  }
-}
-
-function displayForecast(response) {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-  let forecastHtml = "";
-
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">🌤️</div>
-            <div class="weather-forecast-temperatures">
-              <div class="weather-forecast-temperature">
-                <strong>15°</strong>
-              </div>
-              <div class="weather-forecast-temperature">9°</div>
-            </div>
-          </div>
-          `;
-  });
-
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = forecastHtml;
-}
-
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
